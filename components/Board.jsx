3c3f2b4c5bceb8da93bbac3ecac6b1aae03b8572
L_Pareja <leonardo.pareja@publicisgroupe.com>
2022-12-24 10:18:34 -0500

    Add labels to rows

--- components/Board.jsx
+++ components/Board.jsx
@@ -4,7 +4,7 @@ import Cell from "./Cell";
 export default function Board({layout}) {
   return (
     <div className="board">
-      {layout.map((row_item)=><div className="row" key={row_item[0].row}>{row_item.map((cell_item)=><Cell key={cell_item.label} properties={cell_item}></Cell>)}</div>)}
+      {layout.map((row_item)=><div className="row" key={row_item[0].row}><div className="row-label">{row_item[0].row}</div>{row_item.map((cell_item)=><Cell key={cell_item.label} properties={cell_item}></Cell>)}</div>)}
     </div>
   )
 }
